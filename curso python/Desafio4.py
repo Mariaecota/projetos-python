f0fb855633990da7909843c2034e84e7b84ae487
Maria Cota <you@example.com>
2023-06-08 14:57:43 -0300

    Esse é o arquivo 1

---
+++ curso python/Desafio4.py
@@ -0,0 +1,8 @@
+""" Desafio 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+n1 = int(input("Digite um valor: "))
+
+n2 = int(input("Digite outro valor: "))
+s = n1 + n2
+print"""
+
+print(type(True))
