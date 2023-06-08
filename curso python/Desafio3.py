dcc7fe3d887e5944b50587cc5acf169926401bdb
Maria Cota <you@example.com>
2023-06-08 14:58:17 -0300

    tedte

---
+++ curso python/Desafio3.py
@@ -0,0 +1,5 @@
+"""Desafio 003: Crie um programa que leia dois números e mostre a soma entre eles"""
+num1 = int(input("Qual é o número 1? "))
+num2 = int(input("Qual é o número 2? "))
+soma = num1 + num2
+print(f"A soma do {num1} mais a soma do número {num2} é igual a {soma}")
